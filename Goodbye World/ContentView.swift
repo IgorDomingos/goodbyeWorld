b9e90592df8cabbab16994dde13337e81cbcc2b5
Guilherme-Calland <gui.crb0@gmail.com>
2022-09-13 11:35:37 -0300

    denovo

--- Goodbye World/ContentView.swift
+++ Goodbye World/ContentView.swift
@@ -10,13 +10,15 @@ import UIKit
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        
+        Text("Hello, world!").padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
     }
 }
